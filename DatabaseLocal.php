900fb1b320d0976d1e7870900054836d80f16fb1
BuildTools <unconfigured@null.spigotmc.org>
2020-11-10 21:20:04 +1000

    first commit

--- DatabaseLocal.php
+++ DatabaseLocal.php
@@ -1,17 +1,17 @@
 <?php
  
 class database{
-    private $host = 'ec2-54-91-178-234.compute-1.amazonaws.com';
-    private $db_name = 'danh5kcohmgccn';
-    private $username = 'cmutaimxdtwmad';
-    private $password = '9cce09313a2575bca71019baaa1c847c82cf154aa66d0962fbc7b1a2ceea5797';
+    private $host = 'localhost';
+    private $db_name = 'admin';
+    private $username = 'root';
+    private $password = '';
     private $conn;
 
     public function connect(){
         $this->conn = null;
 
         try {
-            $this->conn = new PDO('pgsql:host='. $this->host . ';dbname='. $this->db_name, 
+            $this->conn = new PDO('mysql:host='. $this->host . ';dbname='. $this->db_name, 
             $this->username,$this->password);
 
             $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);

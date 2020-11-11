2a73c4323225181c6c1205c02d1c08ec944a2903
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 22:31:51 +1000

    edited the role register

--- api/login.php
+++ api/login.php
@@ -56,6 +56,7 @@ $data = json_decode(file_get_contents("php://input"));
             $token = JWT::encode($payload,SECRETE_KEY);
     
             $data = ['token' => $token];
+            $response = json_encode($data);
 			echo $data; exit;
 
         }else{

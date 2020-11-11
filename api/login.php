c69b1c6c5767836fad5ede5ee80f8807b03ce1e5
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 22:32:37 +1000

    edited the role register

--- api/login.php
+++ api/login.php
@@ -56,8 +56,7 @@ $data = json_decode(file_get_contents("php://input"));
             $token = JWT::encode($payload,SECRETE_KEY);
     
             $data = ['token' => $token];
-            $response = json_encode($data);
-			echo $data; exit;
+			echo $token; exit;
 
         }else{
             array_push($validationErrors,"Incorrect Login Detials");

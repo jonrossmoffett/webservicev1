ffa108ba750b55ce63909810a4e5d021c81c6f01
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 23:04:37 +1000

    edited the role register

--- api/getPosts.php
+++ api/getPosts.php
@@ -5,7 +5,7 @@ include_once('../jwt.php');
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
 $db = new database;

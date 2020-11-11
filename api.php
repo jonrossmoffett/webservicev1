45b835bb808a3c3a529a314ee1a41c876da80b25
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 20:55:19 +1000

    edited the role register

--- api.php
+++ api.php
@@ -193,7 +193,7 @@ class Api extends Rest{
 
         if($isValidationError == true){
             header("content-type: application/json");
-			$response = json_encode(['errors' => $ValidationErrors[0]]);
+			$response = json_encode(['errors' => [['error' => $ValidationErrors[0]],['error' => $ValidationErrors[0]]] ]);
 			echo $response;exit;
         }
 

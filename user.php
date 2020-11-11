4add035f18788dd4328c30077f2f0ea0e6c9b31c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 18:04:57 +1000

    edited the role register

--- user.php
+++ user.php
@@ -65,8 +65,9 @@
                 $stmt->execute();
                 $user = $stmt->fetch();
                 $id = $user['id'];
+                echo ($id = $user['id']);
 
-                $sql = 'INSERT INTO role_user (role_id, user_id, user_type) VALUES(:role_id, :user_id, :user_type)'; 
+                $sql = 'INSERT INTO role_user (role_id, user_id, user_type) VALUES( :role_id, :user_id, :user_type)'; 
                 $stmt = $this->dbConn->prepare($sql);
                 $stmt->bindParam(':role_id', 3 );
                 $stmt->bindParam(':user_id', $id );

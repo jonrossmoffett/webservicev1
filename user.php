aa8ef1cbb3fc3e193fe86eee4277422299ed9719
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 18:01:01 +1000

    edited the role register

--- user.php
+++ user.php
@@ -66,11 +66,12 @@
                 $user = $stmt->fetch();
                 $id = $user['id'];
 
-/*                 $sql = 'INSERT INTO role_user (user_id, user_type) VALUES(:user_id, :user_type)'; 
+                $sql = 'INSERT INTO role_user (role_id, user_id, user_type) VALUES(:role_id, :user_id, :user_type)'; 
                 $stmt = $this->dbConn->prepare($sql);
+                $stmt->bindParam(':role_id', 3 );
                 $stmt->bindParam(':user_id', $id );
                 $stmt->bindParam(':user_type', 'App\Models\User');
-                $stmt->execute(); */
+                $stmt->execute();
 
                 //http_response_code(404);
                 $this->returnResponse(EMAIL_TAKEN,'User Created'); 

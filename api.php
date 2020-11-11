bf0ab1eb7299980ffcf874d490d4345fe96516f5
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 16:47:36 +1000

    added functionaliy

--- api.php
+++ api.php
@@ -20,28 +20,34 @@ class Api extends Rest{
         $pass = $this->validateParameter('pass',$this->param['pass'], STRING);
 
         try{
-        $stmt = $this->dbConn->prepare("SELECT * FROM users WHERE email = :email AND password = :pass");
+        $stmt = $this->dbConn->prepare("SELECT * FROM users WHERE email = :email");
         $stmt->bindParam(":email", $email);
-        $stmt->bindParam(':pass', $pass);
-
         $stmt->execute();
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        if(!is_array($user)){
+        $user = $stmt->fetch();
+
+        if(password_verify($pass,$user['password'])){
+
+            $payload = [
+                'iat' => time(),
+                'iss' => 'localhost',
+                'exp' => time() + (60 * 60),
+                'userId' => $user['id']
+            ];
+    
+            $token = JWT::encode($payload,SECRETE_KEY);
+    
+            $data = ['token' => $token];
+            $this->returnResponse(SUCCESS_RESPONSE,$data);
+
+        }else{
+            $this->returnResponse(INVALID_USER_PASS,'invald user pass');
+        }   
+
+/*         if(!is_array($user)){
             $logger->debug( $_SERVER['HTTP_USER_AGENT']. " with Ip ". $_SERVER['REMOTE_ADDR'] . " returned error, invalid login credentials" );
             $this->returnResponse(INVALID_USER_PASS, "invalid login credentials");
-        }
+        } */
 
-        $payload = [
-            'iat' => time(),
-            'iss' => 'localhost',
-            'exp' => time() + (60 * 60),
-            'userId' => $user['id']
-        ];
-
-        $token = JWT::encode($payload,SECRETE_KEY);
-
-        $data = ['token' => $token];
-        $this->returnResponse(SUCCESS_RESPONSE,$data);
         
         }catch(Exception $e){
             $this->throwError(JWT_PROCESSING_ERROR, $e->getmessage());

7b0c4099f4e0d4cc0d377242dea51d62171bc8ec
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 00:03:37 +1000

    edited the role register

---
+++ api/createPost.php
@@ -0,0 +1,57 @@
+<?php
+include_once('../database.php');
+include_once('../jwt.php');
+include_once('../authToken.php');
+include_once('../post.php');
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+
+$db = new database;
+$dbConn = $db->connect();
+
+$data = json_decode(file_get_contents("php://input"));
+
+$validationErrors = [];
+$isValidationError = false;
+
+/* $headers = apache_request_headers();
+$Auth = $headers['Authorization'];
+$Auth = ltrim($Auth,"Bearer"); */
+
+$title = $data->title;
+$description = $data->description;
+
+
+$authCheck = new AuthTokenChecker;
+$token = $authCheck->getBearerToken();
+
+$uid = $authCheck->validateToken($token);
+
+try 
+{
+    $post = new Post;
+
+    $post->setTitle($title);
+    $post->setDescription($description);
+    $post->setStatus(0);
+    $post->setCreatedBy($uid);
+    $post->setCreatedAt(date('Y-m-d'));
+    $post->setUpdatedAt(date('Y-m-d'));
+
+    try
+    {
+        $post->insert();
+        $message = "inserted into db";
+    }
+    catch(Exception $e)
+    {
+        $message = $e->getMessage();
+    }
+}
+catch(Exception $e)
+{
+   echo $e->getmessage();exit;
+}

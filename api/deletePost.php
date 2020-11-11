31461320023baa919e86720aab16d6303337a095
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 01:41:18 +1000

    edited the role register

--- api/deletePost.php
+++ api/deletePost.php
@@ -24,7 +24,7 @@ $Auth = ltrim($Auth,"Bearer"); */
 $postId = $data->postId;
 
 $validator = new Validator;
-$validator->validateParameter('PostId',$postId,INTEGER,TRUE,FALSE,FALSE);
+$validator->validateParameter('PostId',$postId,INTEGER,0,0,TRUE,FALSE,FALSE);
 
 
 $authCheck = new AuthTokenChecker;

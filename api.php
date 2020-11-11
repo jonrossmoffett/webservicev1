4fbd5328fe872880333492fdbb52fb9bb0c53748
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 20:38:51 +1000

    added functionaliy

--- api.php
+++ api.php
@@ -191,7 +191,7 @@ class Api extends Rest{
 
         if($isValidationError == true){
             header("content-type: application/json");
-			$response = json_encode(['errors' => $ValidationErrors]);
+			$response = json_encode(['errors' => json_encode($ValidationErrors)]);
 			echo $response;exit;
         }
 

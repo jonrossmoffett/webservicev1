f9f630b0821cdfb2faab603077ee5e48cf982efe
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 22:23:48 +1000

    edited the role register

--- api/login.php
+++ api/login.php
@@ -1,4 +1,8 @@
 <?php
+include_once('../database.php');
+include_once('../jwt.php');
+
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');

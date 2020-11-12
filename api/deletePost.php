417b7341b39942fce76d1ce43479e84a5446b725
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 18:57:11 +1000

    edited delete

--- api/deletePost.php
+++ api/deletePost.php
@@ -4,12 +4,10 @@ include_once('../jwt.php');
 include_once('../authToken.php');
 include_once('../post.php');
 include_once('../validator.php');
+include_once('../vendor/autoload.php');
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-/* header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
- */
 
 if (isset($_SERVER["HTTP_ORIGIN"])) {
     header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
@@ -21,13 +19,17 @@ if (isset($_SERVER["HTTP_ORIGIN"])) {
   if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
     if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     //if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) header("Access-Control-Allow-Headers: {" . $_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"] ."}");
-    http_response_code(200);
+    $request = Request::createFromGlobals();
+    $response = new Response();
+    $response->setStatusCode(200);
+    $response->prepare($request);
+    $response->send();
   }
   header("Content-Type: application/json; charset=UTF-8");
 
 
 $validator = new Validator;
-//$validator->validateRequestType('POST');
+$validator->validateRequestType('POST');
 $db = new database;
 $dbConn = $db->connect();
 $data = json_decode(file_get_contents("php://input"));

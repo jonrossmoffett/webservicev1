e7bb277091b003e7b22d77af3436257b3d19271e
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 14:18:24 +1000

    edited api

--- api/login.php
+++ api/login.php
@@ -4,17 +4,22 @@ include_once('../jwt.php');
 include_once('../constants.php');
 include_once('../validator.php');
 
-//header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-//header('Content-Type: application/json;');
-//header('Access-Control-Allow-Methods: POST,OPTIONS,GET,DELETE');
-//header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
+//header('Access-Control-Allow-Origin: *');
+//header('Content-Type: application/json');
+//header('Access-Control-Allow-Methods: POST');
+//header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With,Referer,User-Agent,Access-Control-Allow-Origin');
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With,Referer,User-Agent,Access-Control-Allow-Origin');
-
+if (isset($_SERVER["HTTP_ORIGIN"])) {
+    header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Max-Age: 0");
+  }
+  if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) header("Access-Control-Allow-Headers: {" . $_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"] ."}");
+  }
+  header("Content-Type: application/json; charset=UTF-8");
 
 
         $db = new database;

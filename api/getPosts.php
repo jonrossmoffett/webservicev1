db3f206477a24f09231e4c71fb8248f5ca657f9b
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 14:55:09 +1000

    edited api

--- api/getPosts.php
+++ api/getPosts.php
@@ -5,10 +5,25 @@ include_once('../authToken.php');
 include_once('../post.php');
 include_once('../validator.php');
 
-header('Access-Control-Allow-Origin: *');
+/* header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With'); */
+if (isset($_SERVER["HTTP_ORIGIN"])) {
+    header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Max-Age: 0");
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With,Referer,User-Agent,Access-Control-Allow-Origin');
+    http_response_code(200);
+  }
+  if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    //if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) header("Access-Control-Allow-Headers: {" . $_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"] ."}");
+    http_response_code(200);
+  }
+  header("Content-Type: application/json; charset=UTF-8");
+
+
 
 $db = new database;
 $dbConn = $db->connect();

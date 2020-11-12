c2c51af19fdac98cb23b7c91f31aa705f602938d
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 14:09:23 +1000

    edited api

--- api/login.php
+++ api/login.php
@@ -12,7 +12,7 @@ include_once('../validator.php');
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
 

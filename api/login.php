c7080bec7551ecbfed60d99380a00e2e61f3cdf6
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 13:53:53 +1000

    edited api

--- api/login.php
+++ api/login.php
@@ -4,11 +4,19 @@ include_once('../jwt.php');
 include_once('../constants.php');
 include_once('../validator.php');
 
-header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-header('Content-Type: application/json;');
-header('Access-Control-Allow-Methods: POST,OPTIONS,GET,DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+//header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+//header('Content-Type: application/json;');
+//header('Access-Control-Allow-Methods: POST,OPTIONS,GET,DELETE');
+//header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
+
+header("Access-Control-Allow-Origin", "*");
+header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if($_SERVER['REQUEST_METHOD'] !== 'OPTIONS'){
+    header('HTTP/1.1 200 ok');
+}
 
         $db = new database;
         $dbConn = $db->connect();

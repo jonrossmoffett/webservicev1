0e057a621dc260790de3c51b41f048983d9646e9
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 14:34:05 +1000

    edited api

--- api/login.php
+++ api/login.php
@@ -18,6 +18,7 @@ if (isset($_SERVER["HTTP_ORIGIN"])) {
   if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
     if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) header("Access-Control-Allow-Headers: {" . $_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"] ."}");
+    http_response_code(200);
   }
   header("Content-Type: application/json; charset=UTF-8");
 

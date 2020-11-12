668ca691e37fb561b035d9f3f44c8366b07cb1c2
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 13:57:45 +1000

    edited api

--- api/login.php
+++ api/login.php
@@ -15,7 +15,7 @@ header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 if($_SERVER['REQUEST_METHOD'] !== 'OPTIONS'){
-    header('HTTP/1.1 200 ok');
+    http_response_code(200);
 }
 
         $db = new database;

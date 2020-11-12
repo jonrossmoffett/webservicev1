94febbcb77e6e45668e7c646fe760834ca7c734b
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 14:03:27 +1000

    edited api

--- api/login.php
+++ api/login.php
@@ -10,18 +10,17 @@ include_once('../validator.php');
 //header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
 
-header("Access-Control-Allow-Origin", "*");
-header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Referer");
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
-if($_SERVER['REQUEST_METHOD'] !== 'OPTIONS'){
-    http_response_code(200);
-}
+
 
         $db = new database;
         $dbConn = $db->connect();
 
-$data = json_decode(file_get_contents("php://input"));
+        $data = json_decode(file_get_contents("php://input"));
         
         $validationErrors = [];
         $isValidationError = false;

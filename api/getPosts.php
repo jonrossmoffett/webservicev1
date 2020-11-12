c2a0ad400a0fe5eb953baabe7a8e9d3aa61555dd
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 18:35:46 +1000

    edited delete

--- api/getPosts.php
+++ api/getPosts.php
@@ -21,10 +21,15 @@ if (isset($_SERVER["HTTP_ORIGIN"])) {
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With,Referer,User-Agent,Access-Control-Allow-Origin');
     http_response_code(200);
   }
+
   if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
     if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     //if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) header("Access-Control-Allow-Headers: {" . $_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"] ."}");
-    http_response_code(200);
+    $request = Request::createFromGlobals();
+    $response = new Response();
+    $response->setStatusCode(200);
+    $response->prepare($request);
+    $response->send();
   }
   header("Content-Type: application/json; charset=UTF-8");
 

5fe49a35a7d3ae15a84b1c7633f20d4168a99662
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 13:01:55 +1000

    edited api

--- api/login.php
+++ api/login.php
@@ -7,7 +7,8 @@ include_once('../validator.php');
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+//header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
         $db = new database;
         $dbConn = $db->connect();

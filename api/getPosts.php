84c0822b6e0987f40787fc911fb38ca3de076661
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 18:37:32 +1000

    edited delete

--- api/getPosts.php
+++ api/getPosts.php
@@ -15,7 +15,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With'); */
 if (isset($_SERVER["HTTP_ORIGIN"])) {
-    header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Max-Age: 0");
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With,Referer,User-Agent,Access-Control-Allow-Origin');
